600421e254d443ef7b4c8c814ff730be89656fde
Daria Tuzova <darituzova@yandex.ru>
2025-11-22 17:49:17 +0300

    Update particle.py
    change speed_x to more range and add if __name__ == "__main__":

--- particle.py
+++ particle.py
@@ -10,7 +10,7 @@ class Particle:
         self.color = color # От фейерверка зависит
         
         # У следующих атрибутов рандомные значения
-        self.speed_x = random.uniform(-2, 2)
+        self.speed_x = random.uniform(-3, 3)
         self.speed_y = random.uniform(-5, 2)
         self.size = random.randint(1, 4)
         self.lifetime = random.randint(30, 90)
@@ -30,42 +30,43 @@ class Particle:
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
 
-# Экран
-pygame.init()
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Частица')
-background = (0, 0, 0)
-clock = pygame.time.Clock()
+if __name__ == "__main__":
+    # Экран
+    pygame.init()
+    width, height = 800, 600
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption('Частица')
+    background = (0, 0, 0)
+    clock = pygame.time.Clock()
 
-# Список всех частиц
-particles = []
+    # Список всех частиц
+    particles = []
 
-# Основной цикл
-running = True
+    # Основной цикл
+    running = True
 
-while running:
-    screen.fill((background))
-    
-    # Обработчик событий
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE: # Клавища ESC - выход
-                running = False
-    
-    new_particle = Particle(random.randint(0, width), random.randint(0, height), (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-    particles.append(new_particle)
-    
-    for particle in particles[:]: # Испольуем копию списка, чтобы безопасно удалять частицы
-        particle.update()
-        particle.draw(screen)
+    while running:
+        screen.fill((background))
         
-        if not particle.is_alive():
-            particles.remove(particle)
-    
-    pygame.display.flip()
-    clock.tick(60)
-    
-pygame.quit()
+        # Обработчик событий
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE: # Клавища ESC - выход
+                    running = False
+        
+        new_particle = Particle(random.randint(0, width), random.randint(0, height), (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        particles.append(new_particle)
+        
+        for particle in particles[:]: # Испольуем копию списка, чтобы безопасно удалять частицы
+            particle.update()
+            particle.draw(screen)
+            
+            if not particle.is_alive():
+                particles.remove(particle)
+        
+        pygame.display.flip()
+        clock.tick(60)
+        
+    pygame.quit()
