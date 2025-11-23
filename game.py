bb3d9a3276354723663cd792f5cd63a34858c299
Daria Tuzova <darituzova@yandex.ru>
2025-11-23 15:13:32 +0300

    base game

---
+++ game.py
@@ -0,0 +1,26 @@
+import pygame
+import random
+from particle import Particle
+from firework import Firework
+
+class Game:
+    def __init__(self):
+        pass
+    
+    def handle_events(self):
+        pass
+    
+    def spawn_random_firework(self):
+        pass
+    
+    def update(self):
+        pass
+    
+    def draw(self):
+        pass
+    
+    def run(self):
+        pass
+
+game = Game()
+game.run()
