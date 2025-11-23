efa3c27bc5ac5c61d0b82a14e6d3f4fcaa4f1a8c
Daria Tuzova <darituzova@yandex.ru>
2025-11-23 15:13:16 +0300

    Update firework.py
    add if __name__ == "__main__":

--- firework.py
+++ firework.py
@@ -59,50 +59,51 @@ class Firework:
         else: # Произошел взрыв
             for particle in self.particles:
                 particle.draw(screen)   
+
+if __name__ == "__main__":        
+    # Экран
+    pygame.init()
+    width, height = 800, 600
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption('Фейерверк')
+    background = (0, 0, 0)
+    clock = pygame.time.Clock()
+
+    # Список всех фейерверков
+    fireworks = []
+
+    # Таймер для фейерверков
+    firework_timer = 0
+    firework_interval = 30
+
+    # Основной цикл
+    running = True
+
+    while running:
+        screen.fill((background))
         
-# Экран
-pygame.init()
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Фейерверк')
-background = (0, 0, 0)
-clock = pygame.time.Clock()
-
-# Список всех фейерверков
-fireworks = []
-
-# Таймер для фейерверков
-firework_timer = 0
-firework_interval = 30
-
-# Основной цикл
-running = True
-
-while running:
-    screen.fill((background))
-    
-    # Обработчик событий
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE: # Клавища ESC - выход
+        # Обработчик событий
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 running = False
-    
-    firework_timer += 1
-    if firework_timer >= firework_interval:
-        new_firework = Firework(random.randint(20, width - 20), height)
-        fireworks.append(new_firework)
-        firework_timer = 0
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE: # Клавища ESC - выход
+                    running = False
         
-    for firework in fireworks[:]: # Испольуем копию списка, чтобы безопасно удалять фейерверки
-        firework.update()
-        firework.draw(screen)
+        firework_timer += 1
+        if firework_timer >= firework_interval:
+            new_firework = Firework(random.randint(20, width - 20), height)
+            fireworks.append(new_firework)
+            firework_timer = 0
             
-        if not firework.is_alive():
-            fireworks.remove(firework)
-    
-    pygame.display.flip()
-    clock.tick(60)
-    
-pygame.quit()
+        for firework in fireworks[:]: # Испольуем копию списка, чтобы безопасно удалять фейерверки
+            firework.update()
+            firework.draw(screen)
+                
+            if not firework.is_alive():
+                fireworks.remove(firework)
+        
+        pygame.display.flip()
+        clock.tick(60)
+        
+    pygame.quit()
